989c40bafd161996fe55798326b7d9ebaef21d65
hadenyoyo <10506594+hadenyoyo@users.noreply.github.com>
2024-03-18 14:16:14 -0600

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module gopomo
+
+go 1.22.1
---
+++ src/gopomo.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+)
+
+const LOOPS_AFTER_BREAK = 3
+
+func main() {
+	var argc int = len(os.Args)
+
+	if argc != 3 {
+		fmt.Printf("Usage: %s <work-time(m)> <break-time(m)>\n", os.Args[0])
+		os.Exit(0)
+	}
+
+	// Parse arguments
+	workStr := os.Args[1]
+	workTime, err := time.ParseDuration(workStr)
+	if err != nil {
+		fmt.Printf("Invalid work time format: %v\n", err)
+		os.Exit(1)
+	}
+
+	breakStr := os.Args[2]
+	breakTime, err := time.ParseDuration(breakStr)
+	if err != nil {
+		fmt.Printf("Invalid break time format: %v\n", err)
+		os.Exit(1)
+	}
+
+	var isBreak bool = false
+	var longBreakTimer int = 0
+
+	for {
+		if !isBreak {
+			fmt.Println("Starting Work.")
+			ticker := time.NewTicker(1 * time.Second)
+			startTime := time.Now()
+
+			go func() {
+				for range ticker.C {
+					elapsed := time.Since(startTime)
+					remaining := workTime - elapsed
+					if remaining <= 0 {
+						ticker.Stop()
+						return
+					}
+					fmt.Printf("Remaining work time: %v\n", remaining.Round(time.Second))
+				}
+			}()
+
+			time.Sleep(workTime)
+			fmt.Println("Work is Over.")
+			isBreak = true
+		} else if isBreak && longBreakTimer != LOOPS_AFTER_BREAK {
+			fmt.Println("Starting Break.")
+			ticker := time.NewTicker(1 * time.Second)
+			startTime := time.Now()
+
+			go func() {
+				for range ticker.C {
+					elapsed := time.Since(startTime)
+					remaining := breakTime - elapsed
+					if remaining <= 0 {
+						ticker.Stop()
+						return
+					}
+					fmt.Printf("Remaining break time: %v\n", remaining.Round(time.Second))
+				}
+			}()
+
+			time.Sleep(breakTime)
+			fmt.Println("Break is Over.")
+			isBreak = false
+			longBreakTimer++
+		} else if isBreak && longBreakTimer == LOOPS_AFTER_BREAK {
+			fmt.Println("Starting Long Break.")
+			ticker := time.NewTicker(1 * time.Second)
+			startTime := time.Now()
+
+			go func() {
+				for range ticker.C {
+					elapsed := time.Since(startTime)
+					remaining := (breakTime * 2) - elapsed
+					if remaining <= 0 {
+						ticker.Stop()
+						return
+					}
+					fmt.Printf("Remaining long break time: %v\n", remaining.Round(time.Second))
+				}
+			}()
+
+			time.Sleep(breakTime * 2)
+			ticker.Stop()
+			fmt.Println("Long Break is Over.")
+			isBreak = false
+			longBreakTimer = 0
+		}
+
+	}
+}
